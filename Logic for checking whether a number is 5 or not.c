1de4b6b7fb46299c0c316627dd61473394bb9eef
MUSLIM UDDIN <144557545+muslimuddin2002@users.noreply.github.com>
2024-02-15 00:19:41 +0600

    Create Logic for checking whether a number is 5 or not

---
+++ Logic for checking whether a number is 5 or not.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a==5){
+        printf("number is 5");
+    }
+    if(a!=5){
+        printf("number is not 5");
+    }
+    return 0;
+}
