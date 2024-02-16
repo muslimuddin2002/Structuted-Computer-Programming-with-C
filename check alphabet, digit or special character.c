9136fb38a7dc24a4f67f8e566ecc20dcf8429c34
MUSLIM UDDIN <144557545+muslimuddin2002@users.noreply.github.com>
2024-02-16 11:05:45 +0600

    Create check alphabet, digit or special character

---
+++ check alphabet, digit or special character.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter any character:");
+    scanf("%c",&ch);
+    if((ch>='a' && ch<='z') || (ch>='A') && (ch<='Z')){
+        printf("'%c' is alphabet\n",ch);
+    }
+    else if(ch>='0' && ch<='9'){
+        printf("'%c' is digit\n",ch);
+    }
+    else{printf("'%c' is special character",ch);
+    }
+    return 0;
+}
