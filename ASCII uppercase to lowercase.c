6bf1abb8f35c42556c9e0a18040bab676038b1b5
MUSLIM UDDIN <144557545+muslimuddin2002@users.noreply.github.com>
2024-03-04 17:28:19 +0600

    Create ASCII uppercase to lowercase

---
+++ ASCII uppercase to lowercase.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char upper;
+    printf("Enter any ASCII upperercase letter : ");
+    scanf("%c",&lower);
+    printf("The ASCII lowercase letter is : %c",upper+32);
+    return 0;
+
+}
